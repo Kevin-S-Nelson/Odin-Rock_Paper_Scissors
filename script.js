000aab8bf4cb4a7c7840af67dae3439475dabf5c
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-03 05:14:46 -0600

    Assign a rock, paper, and scissors to 0, 1, and 2 respectivly

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 function getComputerChoice() {
     // Random number between 1 and 3 (will actually be between 0 and 2)
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
+    if (randomNumber == 0) {
+        console.log("Rock");
+    } else if (randomNumber == 1) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
     // Define assign the value of each number to a string
     // return either "Rock", "Paper", or "Scissors"
 }
 
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
 getComputerChoice();
 
 // Create a function that plays one round of Rock Paper Scissors
