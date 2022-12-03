2cb1f8e6e552dd5f693efceaeb6800b73c5850ce
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-03 05:09:33 -0600

    Create a random number between 0 and 2 to be used as rock paper and scissors

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Create a function that gets the computers choice
 function getComputerChoice() {
+    // Random number between 1 and 3 (will actually be between 0 and 2)
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+    // Define assign the value of each number to a string
     // return either "Rock", "Paper", or "Scissors"
 }
+
+getComputerChoice();
 
 // Create a function that plays one round of Rock Paper Scissors
 // The function should take two values 1) playerSelection and 2) computerSelection
